0b6b02b3734dbbfb338f11a6243654f6013b580d
gutamuw <129604444+gutamuw@users.noreply.github.com>
2024-12-09 15:04:49 +0100

    Random number-spel

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,28 @@ const app = document.getElementById("app");
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 
+const randomNumber:number = Math.floor(Math.random() * 100)+1;
+console.log(randomNumber)
+
 form?.addEventListener("submit", (e) => {
   e.preventDefault() 
 
-  let num:number = 0;
+  let inputNumber:number = 0;
 
   if(input) {
-    num = +(input as HTMLInputElement).value;
+    inputNumber = +(input as HTMLInputElement).value;
   }
 
-  if (num < 100) {
-    alert("Det var ett litet tal!")
-  } else {
-    alert("Det var ett stort tal!")
+while(randomNumber !== inputNumber){
+  if(randomNumber > inputNumber){
+    alert("För litet.")
+   
+  } else if(randomNumber < inputNumber){
+    alert("För stort.")
   }
-  
+}
+
+if(randomNumber === inputNumber){
+  alert("Du vann.")
+}
 })
